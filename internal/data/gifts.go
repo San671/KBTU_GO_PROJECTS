35213117d8a2e9c54fef6a1c5211f8414c81df91
ЫÑSan671 <git@github.com:San671/Personalization_gifts.git>
2023-11-22 17:10:22 +0600

    Setting up the Gift Model

--- internal/data/gifts.go
+++ internal/data/gifts.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"personalized_gifts.sanzhar.net/internal/validator"
 	"time"
 )
@@ -29,4 +30,29 @@ func ValidateGift(v *validator.Validator, gift *Gift) {
 	v.Check(gift.Superiority != "", "superiority", "must be provided")
 	v.Check(gift.Status != "", "status", "must be provided")
 	v.Check(gift.Category != "", "category", "must be provided")
+}
+
+// Define a MovieModel struct type which wraps a sql.DB connection pool.
+type GiftModel struct {
+	DB *sql.DB
+}
+
+// Add a placeholder method for inserting a new record in the movies table.
+func (m GiftModel) Insert(gift *Gift) error {
+	return nil
+}
+
+// Add a placeholder method for fetching a specific record from the movies table.
+func (m GiftModel) Get(id int64) (*Gift, error) {
+	return nil, nil
+}
+
+// Add a placeholder method for updating a specific record in the movies table.
+func (m GiftModel) Update(gift *Gift) error {
+	return nil
+}
+
+// Add a placeholder method for deleting a specific record from the movies table.
+func (m GiftModel) Delete(id int64) error {
+	return nil
 }
