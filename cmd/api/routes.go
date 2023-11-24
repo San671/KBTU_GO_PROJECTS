7e3b2a262195fcbf9018b5a117b0d0b884f09b9f
ЫÑSan671 <git@github.com:San671/Personalization_gifts.git>
2023-11-24 22:30:42 +0600

    Parsing Query String Parameters

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,14 +11,13 @@ func (app *application) routes() *httprouter.Router {
 	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
 	// Not Found responses.
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
-	// it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	// Add the route for the GET /v1/movies endpoint.
+	router.HandlerFunc(http.MethodGet, "/v1/gifts", app.listGiftsHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/gifts", app.createGiftHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/gifts/:id", app.showGiftHandler)
-	// Require a PATCH request, rather than PUT.
 	router.HandlerFunc(http.MethodPatch, "/v1/gifts/:id", app.updateGiftHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/gifts/:id", app.deleteGiftHandler)
 	return router
