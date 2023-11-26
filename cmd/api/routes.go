0af64b2660601fda4e4a9551f90eb870f9a70b11
ЫÑSan671 <git@github.com:San671/Personalization_gifts.git>
2023-11-26 20:18:31 +0600

    Panic Recovery

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,20 +5,17 @@ import (
 	"net/http"
 )
 
-func (app *application) routes() *httprouter.Router {
+// Update the routes() method to return a http.Handler instead of a *httprouter.Router.
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	// Convert the notFoundResponse() helper to a http.Handler using the
-	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
-	// Not Found responses.
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
-
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	// Add the route for the GET /v1/movies endpoint.
 	router.HandlerFunc(http.MethodGet, "/v1/gifts", app.listGiftsHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/gifts", app.createGiftHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/gifts/:id", app.showGiftHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/gifts/:id", app.updateGiftHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/gifts/:id", app.deleteGiftHandler)
-	return router
+	// Wrap the router with the panic recovery middleware.
+	return app.recoverPanic(router)
 }
